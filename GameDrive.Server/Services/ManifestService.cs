36d9e526c33e289b570cdb00868af3eb8c763522
Brodie Pestell <brodie.pestell7@gmail.com>
2023-05-27 13:40:34 +0100

    Update manifest generation to account for possibility of deleted storage objects

--- GameDrive.Server/Services/ManifestService.cs
+++ GameDrive.Server/Services/ManifestService.cs
@@ -36,12 +36,30 @@ public class ManifestService
 
     private ManifestFileReport CompareManifestEntry(ManifestEntry entry, StorageObject? storageObject)
     {
+        // If the storage object does not exist then the client-side file must be new
         if (storageObject is null)
             return new ManifestFileReport(FileUploadState.UploadRequested, FileDiffState.New);
+
+        if (storageObject.IsDeleted)
+        {
+            if (storageObject.DeletedDate is null)
+            {
+                // StorageObject.DeletedDate should NEVER be null if StorageObject.IsDeleted is true - what happened?
+                _logger.LogError("StorageObject.DeletedDate is null for StorageObject ID {StorageObjectId}", storageObject.Id);
+                return new ManifestFileReport(FileUploadState.Conflict, FileDiffState.Conflict);
+            }
+            
+            return entry.LastModifiedDate.CompareTo(storageObject.DeletedDate) switch
+            {
+                < 0 => new ManifestFileReport(FileUploadState.Ignore, FileDiffState.Removed), // Last modified BEFORE the deleted date, no need to upload
+                > 0 => new ManifestFileReport(FileUploadState.UploadRequested, FileDiffState.Newer), // Last modified AFTER the deleted date, client-side must be newer
+                _ => new ManifestFileReport(FileUploadState.Conflict, FileDiffState.Conflict), // Removed at exactly the same time as it was updated - raise a conflict.
+            };
+        }
         
         if (entry.FileHash == storageObject.FileHash)
             return new ManifestFileReport(FileUploadState.Ignore, FileDiffState.Same);
-
+        
         return entry.LastModifiedDate.CompareTo(storageObject.LastModifiedDate) switch
         {
             > 0 => new ManifestFileReport(FileUploadState.UploadRequested, FileDiffState.Newer),
