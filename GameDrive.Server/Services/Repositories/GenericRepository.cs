50372c11fac2ef5ca87d848009082e625490260c
Brodie Pestell <brodie.pestell7@gmail.com>
2023-07-02 15:02:49 +0100

    Move IGenericRepository to separate file

--- GameDrive.Server/Services/Repositories/GenericRepository.cs
+++ GameDrive.Server/Services/Repositories/GenericRepository.cs
@@ -4,18 +4,6 @@ using Microsoft.EntityFrameworkCore;
 
 namespace GameDrive.Server.Services.Repositories;
 
-public interface IGenericRepository<T> where T : class
-{
-    Task AddAsync(T entity);
-    Task AddRangeAsync(IEnumerable<T> entities);
-    Task<IReadOnlyCollection<T>> FindAsync(Expression<Func<T, bool>> expression);
-    Task<IReadOnlyCollection<T>> GetAllAsync();
-    Task UpdateAsync(T entity);
-    Task RemoveAsync(T entity);
-    Task RemoveRangeAsync(IEnumerable<T> entities);
-    Task SaveChangesAsync();
-}
-
 public abstract class GenericRepository<T> : IGenericRepository<T> where T : class
 {
     private readonly GameDriveDbContext _dbContext;
