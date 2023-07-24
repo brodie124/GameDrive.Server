bfb6c2cec40d5f6ebd2f018e1576504907251046
Brodie Pestell <brodie.pestell7@gmail.com>
2023-07-24 23:44:35 +0100

    Add missed file

---
+++ GameDrive.Server/Services/Storage/TemporaryStorageProvider.cs
@@ -0,0 +1,51 @@
+using GameDrive.Server.Models.Options;
+using Microsoft.Extensions.Options;
+
+namespace GameDrive.Server.Services.Storage;
+
+public class TemporaryStorageProvider
+{
+    private readonly TemporaryStorageOptions _temporaryStorageOptions;
+
+    public TemporaryStorageProvider(
+        IOptions<TemporaryStorageOptions> temporaryStorageOptions
+    )
+    {
+        _temporaryStorageOptions = temporaryStorageOptions.Value;
+    }
+
+    public async Task<SaveFileResult> SaveFileAsync(Stream source)
+    {
+        // TODO: do this on application startup rather than every time a file is saved...
+        if (!Directory.Exists(_temporaryStorageOptions.TemporaryStoragePath))
+            Directory.CreateDirectory(_temporaryStorageOptions.TemporaryStoragePath);
+        
+        var key = Guid.NewGuid();
+        var relativePath = MakeTemporaryPath(key);
+        var filePath = Path.Combine(Directory.GetCurrentDirectory(), relativePath);
+        await using var writeStream = new StreamWriter(filePath);
+        await source.CopyToAsync(writeStream.BaseStream);
+        await writeStream.FlushAsync();
+        return new SaveFileResult(key);
+    }
+    
+    public Stream GetFile(Guid key)
+    {
+        var relativePath = MakeTemporaryPath(key);
+        var filePath = Path.Combine(Directory.GetCurrentDirectory(), relativePath);
+        if (!File.Exists(filePath))
+            throw new InvalidOperationException("Specified key cannot be found in temp storage");
+        
+        var readStream = new StreamReader(filePath);
+        return readStream.BaseStream;
+    }
+
+    private string MakeTemporaryPath(Guid key)
+    {
+        return Path.Combine(_temporaryStorageOptions.TemporaryStoragePath, $"{key.ToString()}.blob");
+    } 
+
+    public record SaveFileResult(
+        Guid Key
+    );
+}
